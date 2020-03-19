ce0a311101abc74bb7423da4a8e07673585b64d8
Yoel <cyoelten@gmail.com>
2020-03-19 15:46:20 -0300

    Cargamos las paginas con el motor de vista hbs y creamos partials

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 const hbs = require("hbs");
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
 
 let port = process.env.PORT || 3000;
 
+//Creamos un middleware para la carpeta public
+app.use( express.static( __dirname + "/public"));
+//Establecemos como motor de vista predeterminado hbs
+app.set("view engine", "hbs")
+
+//Cargamos todos los parciales que hay en la carpeta "parciales"
+hbs.registerPartials( __dirname + "/views/parciales");
+
 app.get("/", ( req, res ) => {
-    res.write("hola perro");
-    res.end();
+
+    res.render("index");
+
 })
+app.get("/ayuda", ( req, res ) => {
+
+    res.render("ayuda");
+
+})
+app.get("/ubicacion", ( req, res ) => {
+
+    res.render("ubicacion");
+
+})
+app.get("/articulo", ( req, res ) => {
+
+    res.render("articulo");
+
+})
+
 
 app.listen( port, () => {
     console.log(`El server se esta ejecutando en el puerto ${ port }`);

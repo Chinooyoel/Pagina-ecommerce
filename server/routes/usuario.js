0ce57e25f03506e4ee439162e7dcb8d0a294b9fc
chinoo <cyoelten@gmail.com>
2021-07-10 10:07:29 -0300

    Completamos la funcionalidad agregar vendedores

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const { verificarAdminRole, verificarRole, obtenerUsuarioLoguiado } = require("../middleware/autenticacion");
-const { crearUsuario, actualizarUsuario, buscarUsuariosPorEmail, mostrarTablaDeUsuarios, obtenerPerfilUsuarioPorId, actualizarRolUsuario } = require("../controller/usuarioController");
+const { crearUsuario, actualizarUsuario, buscarUsuariosPorEmail, mostrarTablaDeUsuarios, obtenerPerfilUsuarioPorId, actualizarRolUsuario, verCrearVendedor } = require("../controller/usuarioController");
 const { validarRegistrarse } = require("../middleware/validaciones");
 
 // /usuario/
 router.get("/" , verificarRole, mostrarTablaDeUsuarios)
 
-//registrarse
-// /usuario/crear
-router.post('/crear', validarRegistrarse, crearUsuario);
-
-//Para que el admin pueda actualizar al usuario del cliente
-// /usuario/actualizar/:id
-router.post('/actualizar/:id', verificarAdminRole, actualizarUsuario)
+//registra vendedor
+// /usuario/
+router.get("/crear-vendedor" , validarRegistrarse, verCrearVendedor)
 
 //Para buscar a los usuarios por el email
 // /usuario/buscar/:data
@@ -22,6 +18,14 @@ router.get("/buscar/:data", verificarRole , buscarUsuariosPorEmail)
 //obtener perfil del usuario, loguiado como admin
 // /usuario/perfil.:id
 router.get("/perfil/:id", obtenerPerfilUsuarioPorId)
+
+//registrarse
+// /usuario/crear
+router.post('/crear', validarRegistrarse, crearUsuario);
+
+//Para que el admin pueda actualizar al usuario del cliente
+// /usuario/actualizar/:id
+router.post('/actualizar/:id', verificarAdminRole, actualizarUsuario)
 
 //actualizar rol del usuario
 // /usuario/actualizar-rol/:idusuario
